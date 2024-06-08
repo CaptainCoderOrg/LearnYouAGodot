5008a3d3d54edffac613f628e5c2e8962b794c75
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-06-08 05:02:02 -0700

    chore: Add player walking animation.

--- LearnYouAGodot/Scripts/PlayerController.gd
+++ LearnYouAGodot/Scripts/PlayerController.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @onready var ray_cast = $RayCast2D
-@onready var animated_sprite = $Sprite2D
+@onready var animated_sprite = $AnimatedSprite2D
 
 @export var STATS: PlayerStats
 @export var SPEED = 300.0
@@ -16,6 +16,19 @@ func rotate_sprite():
 		animated_sprite.rotation = lerp_angle(animated_sprite.rotation, new_rotation, 0.1)
 	else:
 		animated_sprite.rotation = lerp_angle(animated_sprite.rotation, 0, 0.1)
+
+func update_facing():
+	if velocity.x < 0:
+		animated_sprite.flip_h = true
+	elif velocity.x > 0:
+		animated_sprite.flip_h = false
+
+func update_animation():
+	if velocity.x == 0:
+		animated_sprite.animation = "idle"
+	else:
+		animated_sprite.animation = "walk"
+	animated_sprite.play()
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -34,6 +47,8 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	update_animation()
+	update_facing()
 	rotate_sprite()
 	move_and_slide()
 	
