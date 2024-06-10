faaf79338f57f05bbbd8574bd63649f03f8d6abc
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-06-10 06:46:25 -0700

    chore: Add run + coyote frames.

--- LearnYouAGodot/Scripts/MovementController.gd
+++ LearnYouAGodot/Scripts/MovementController.gd
@@ -4,11 +4,15 @@ extends Node
 @export var character : CharacterBody2D
 @export var direction : float
 @export var SPEED : float
+var multiplier : float = 1
 
 signal on_collision(collision : KinematicCollision2D)
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 static var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func speed():
+	return SPEED * multiplier
 
 func set_direction(p_direction : float):
 	direction = p_direction
@@ -22,9 +26,9 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.	
 	if direction:
-		character.velocity.x = direction * SPEED
+		character.velocity.x = direction * speed()
 	else:
-		character.velocity.x = move_toward(character.velocity.x, 0, SPEED)
+		character.velocity.x = move_toward(character.velocity.x, 0, speed())
 	
 	if character.move_and_slide():
 		for c_ix in character.get_slide_collision_count():
--- LearnYouAGodot/Scripts/PlayerController.gd
+++ LearnYouAGodot/Scripts/PlayerController.gd
@@ -5,6 +5,10 @@ extends Node
 @export var character: CharacterBody2D
 @export var JUMP_VELOCITY = -400.0
 @export var movementController : MovementController
+@export var run_mulitiplier = 1.2
+@export var coyote_frames = 8
+var jump_frames = 2
+
 
 signal direction_changed(direction : float)
 
@@ -13,10 +17,24 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready():
 	direction_changed.connect(movementController.set_direction)
+	
+func _process(delta):
+	if Input.is_action_pressed("run"):
+		movementController.multiplier = run_mulitiplier
+	else:
+		movementController.multiplier = 1
+
+func can_jump():
+	return jump_frames > 0
 
 func _physics_process(delta):
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and character.is_on_floor():
+	if character.is_on_floor():
+		jump_frames = coyote_frames
+	else:
+		jump_frames -= 1
+
+	if Input.is_action_just_pressed("ui_accept") and can_jump():
 		character.velocity.y = JUMP_VELOCITY
-		
+		jump_frames = 0
+	
 	direction_changed.emit(Input.get_axis("ui_left", "ui_right"))
